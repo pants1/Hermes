fe3490fe783f7aaf910c12561fa0df09c0a2faeb
pants1 <dwarvenplague@gmail.com>
2018-09-24 17:53:18 +0800

    Improved comments

--- x/boot_hello_world.asm
+++ x/boot_hello_world.asm
@@ -5,12 +5,12 @@ mov al, 'e'
 int 0x10
 mov al, 'l'
 int 0x10
-int 0x10 ; 'l' is still on al, remember?
+int 0x10 ; 'l' is still on al
 mov al, 'o'
 int 0x10
 
 jmp $ ; jump to current address = infinite loop
 
-; padding and magic number
+; padding and boot number
 times 510 - ($-$$) db 0
 dw 0xaa55
